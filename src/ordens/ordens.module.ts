a39768ff1d1c7cf5cc431310779d2cefbff2420d
Luw17 <luisflavio360@gmail.com>
2025-06-15 13:23:11 -0300

    padronização de rotas e modulos

--- src/ordens/ordens.module.ts
+++ src/ordens/ordens.module.ts
@@ -3,10 +3,11 @@ import { OrdensService } from "./ordens.service";
 import { DatabaseModule } from "../database/database.module";
 import { EmailModule } from "../email/email.module";
 import { UsersModule } from "../users/users.module";
+import { OrdensController } from './ordens.controller';
 
 @Module({
     imports: [DatabaseModule, EmailModule,UsersModule],
-    controllers: [],
+    controllers: [OrdensController],
     providers: [OrdensService],
     exports: [OrdensService]
 })
