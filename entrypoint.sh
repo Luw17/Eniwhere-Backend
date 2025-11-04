4818b5df0564928fe44d82cc6578d2c35354649b
Luw17 <luisflavio360@gmail.com>
2025-11-04 14:09:59 -0300

    feat: removendo o entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ "$NODE_ENV" = "production" ]; then
-  echo "Backend: Running in production mode"
-  exec node dist/main.js
-else
-  echo "Backend: Running in development mode"
-  npm install
-  exec npm run start:dev
-fi
